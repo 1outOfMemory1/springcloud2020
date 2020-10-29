0563f8a0cd82b6add6f386a90d770c5a375736cc
Bob <99611400tc@gmail.com>
2020-10-29 22:47:15 +0800

    完成了对gateway的小demo的配置 可以通过9527端口对8001端口的服务进行访问 以后8001端口就会隐藏起来  只保留9527 更安全

---
+++ cloud-gateway-gateway9527/src/main/java/tech/haonan/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package tech.haonan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: yhn
+ * @Date: 10/29/2020 10:02 PM
+ * @Description:
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
