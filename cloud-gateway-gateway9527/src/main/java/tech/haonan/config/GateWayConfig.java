1b069895d1248aec3a8a0f9cf8e9557cf5b70e56
Bob <99611400tc@gmail.com>
2020-10-30 09:34:49 +0800

    完成了入门配置的第二种网关配置的实现 通过一个gatewayconfig类来实现配置

---
+++ cloud-gateway-gateway9527/src/main/java/tech/haonan/config/GateWayConfig.java
@@ -0,0 +1,27 @@
+package tech.haonan.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: yhn
+ * @Date: 10/30/2020 8:52 AM
+ * @Description:
+ **/
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // 转发 :9527的请求 /payment/getById/**  到http://localhost:8001/payment/getById
+                .route("path_route1", r -> r.path("/payment/getById/**")
+                        .uri("http://localhost:8001"))
+                // 转发 :9527的请求 /payment/lb/**  到http://localhost:8001/payment/lb/**
+                .route("path_route2", r -> r.path("/payment/lb/**")
+                        .uri("http://localhost:8001")).build();
+    }
+}
+
+
