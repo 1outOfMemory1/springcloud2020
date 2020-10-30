bc735a22dd39f6ff46d901b1508d1510d4d6768a
Bob <99611400tc@gmail.com>
2020-10-30 13:35:39 +0800

    使用gateway对8001 8002 两个微服务进行负载均衡 而且不暴露着两个端口

--- cloud-gateway-gateway9527/src/main/java/tech/haonan/config/GateWayConfig.java
+++ cloud-gateway-gateway9527/src/main/java/tech/haonan/config/GateWayConfig.java
@@ -12,16 +12,19 @@ import org.springframework.context.annotation.Configuration;
  **/
 @Configuration
 public class GateWayConfig {
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                // 转发 :9527的请求 /payment/getById/**  到http://localhost:8001/payment/getById
-                .route("path_route1", r -> r.path("/payment/getById/**")
-                        .uri("http://localhost:8001"))
-                // 转发 :9527的请求 /payment/lb/**  到http://localhost:8001/payment/lb/**
-                .route("path_route2", r -> r.path("/payment/lb/**")
-                        .uri("http://localhost:8001")).build();
-    }
+    //静态路由
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                // 转发 :9527的请求 /payment/getById/**  到http://localhost:8001/payment/getById
+//                .route("path_route1", r -> r.path("/payment/getById/**")
+//                        .uri("http://localhost:8001"))
+//                // 转发 :9527的请求 /payment/lb/**  到http://localhost:8001/payment/lb/**
+//                .route("path_route2", r -> r.path("/payment/lb/**")
+//                        .uri("http://localhost:8001")).build();
+//    }
+    // 动态路由
+
 }
 
 
